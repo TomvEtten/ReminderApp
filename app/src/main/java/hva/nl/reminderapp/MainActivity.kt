0c0ca1c2920991ba66eaa36a0e35e6476f72e265
tomvanetten <tomvanetten@outlook.com>
2019-09-09 13:16:11 +0200

    commit swipe

--- app/src/main/java/hva/nl/reminderapp/MainActivity.kt
+++ app/src/main/java/hva/nl/reminderapp/MainActivity.kt
@@ -3,6 +3,7 @@ package hva.nl.reminderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-
+        createItemTouchHelper().attachToRecyclerView(rvReminder)
         // Initialize the recycler view with a linear layout manager, adapter
         rvReminder.layoutManager =
             LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
@@ -48,6 +49,32 @@ class MainActivity : AppCompatActivity() {
         reminderAdapter.notifyDataSetChanged()
         etReminder.text?.clear()
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
 
 
 }
